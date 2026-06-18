b3481898f1c1184fbb72989faf5706a89516b9c3
ISHITA Sirohi <ishitasirohi0@gmail.com>
2026-06-19 01:40:16 +0530

    Day17 uploaded

---
+++ Day17/Q66.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr1[] = {1,2,3,4};
+    int arr2[] = {3,4,5,6};
+
+    for(int i=0;i<4;i++)
+        cout << arr1[i] << " ";
+
+    for(int i=0;i<4;i++) {
+
+        bool found=false;
+
+        for(int j=0;j<4;j++) {
+            if(arr2[i]==arr1[j]) {
+                found=true;
+                break;
+            }
+        }
+
+        if(!found)
+            cout << arr2[i] << " ";
+    }
+
+    return 0;
+}
